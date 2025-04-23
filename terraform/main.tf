a7b43ef7c06936cd66ce2bf7f1631f6e2bbc680e
janicki77 <matthew.janicki0@gmail.com>
2025-04-23 09:16:13 +0200

    add delegation

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,6 +42,18 @@ resource "azurerm_subnet" "appservice_subnet" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+    }
+  }
 }
 
 resource "azurerm_app_service_virtual_network_swift_connection" "app_vnet" {
