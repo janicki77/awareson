9e0de4cb3933d47722fe63f818db8ab375687335
janicki77 <matthew.janicki0@gmail.com>
2025-04-23 06:38:08 +0200

    var name fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_container_registry" "acr" {
   name                = "${var.prefix}acr"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   sku                 = "Basic"
   admin_enabled       = true
 }
